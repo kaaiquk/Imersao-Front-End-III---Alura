375ee7656da6a1b85e5b18016927f20a5422b98d
unknown <kaiquedos2303@gmail.com>
2026-03-30 17:50:48 -0300

    função do localstorage e adição de filmes ao catalogo

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -11,31 +11,52 @@ export const categories = [
                 youtube: "https://www.youtube.com/watch?v=kQUPdVxZNPk"
             },
             {
-                img: "../assets/martysupreme.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=s9gSuKaKcqM"
+                img: "../assets/devoradores.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=uI1KofmBtYI"
             },
             {
                 img: "../assets/pecadores.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=e9kwQahD8YY"
             },
+            {
+                img: "../assets/bleedforthis.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=1dungokOPTw"
+            },
+        ]
+    },
+    {
+        title: "Continuar assistindo",
+        items: [
+            { img: "../assets/arcane.png", top10: true, progress: 52, youtube: "https://www.youtube.com/watch?v=ysqiEC6bLUI" },
+            { img: "../assets/jujutsukaisen.png", badge: "Novo episódio", badgeColor: "red", progress: 35, youtube: "https://www.youtube.com/watch?v=MPfZhgLiK6w" },
+            { img: "../assets/homemaranha.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", progress: 72, youtube: "https://www.youtube.com/watch?v=Etv-L2JKCWk" },
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "../assets/theoffice.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=tNcDHWpselE" },
+            { img: "../assets/arcane.png", top10: true, youtube: "https://www.youtube.com/watch?v=ysqiEC6bLUI" },
+            { img: "../assets/pluribus.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=a6lzvWby9UE" },
+            { img: "../assets/breakingbad.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=VFkjBy2b50Q" },
         ]
     },
     {
-        title: "Para maratonar",
+        title: "Animes",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "../assets/lookback.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wkRR2VUsWfU" },
+            { img: "../assets/yourname.png", top10: true, youtube: "https://www.youtube.com/watch?v=soQXM3XVvIU" },
+            { img: "../assets/jujutsukaisen.png", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=MPfZhgLiK6w" },
+            { img: "../assets/onepiece.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=YFbno_aPm0w&list=RDYFbno_aPm0w&start_radio=1" },
         ]
-    }
+    },
+    {
+        title: "Super-heróis",
+        items: [
+            { img: "../assets/homemaranha.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Etv-L2JKCWk" },
+            { img: "../assets/4fantastico.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=P273sRlm4tM" },
+            { img: "../assets/superman.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=14qzQDMcTqM" },
+            { img: "../assets/esquadraosuicida.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=h6bWwRCtg28" },
+        ]
+    },
 ];
